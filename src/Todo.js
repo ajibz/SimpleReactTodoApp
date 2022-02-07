43688b9e5a243ba36c1f9058e2325e1a0115094d
ajibz <48716414+ajibz@users.noreply.github.com>
2022-02-07 21:48:06 +0100

    Delete operation

--- src/Todo.js
+++ src/Todo.js
@@ -3,15 +3,9 @@ import React, { useState } from "react";
 export default function Todo() {
   const [todoInput, setTodoInput] = useState("");
   const [todo, setTodo] = useState([]);
+  const [check, setCheck] = useState();
   return (
-    <div>
-      {todo.map((e) => {
-        return (
-          <ul>
-            <li>{e}</li>
-          </ul>
-        );
-      })}
+    <div style={{ marginTop: 20 }}>
       <label style={{ fontSize: 23, fontWeight: "bold" }}>Enter Todo: </label>
       <input
         onChange={(e) => {
@@ -37,6 +31,44 @@ export default function Todo() {
             setTodoInput("");
           }}
         />
+      </div>
+
+      <div style={{ marginTop: 50 }}>
+        {todo.map((e) => {
+          return (
+            <ul style={{ listStyle: "none" }}>
+              <li
+                style={{
+                  borderStyle: "solid",
+                  borderColor: "orange",
+                  borderWidth: 1,
+                  borderRadius: 5,
+                  fontSize: 21,
+                  width: "80%"
+                }}
+              >
+                <input
+                  type="checkbox"
+                  onChange={() => {
+                    var index = todo.indexOf(e);
+
+                    if (index !== -1) {
+                      var newTodo = todo.splice(index, 1);
+                      setTodo([newTodo]);
+                    }
+
+                    // setCheck(true);
+
+                    // if(check === true){
+
+                    // }
+                  }}
+                />{" "}
+                {e}
+              </li>
+            </ul>
+          );
+        })}
       </div>
     </div>
   );
