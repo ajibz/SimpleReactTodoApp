2ed1ff7fd7ed720ab04f781fb0ff895c15db7ba3
ajibz <48716414+ajibz@users.noreply.github.com>
2022-02-08 15:00:16 +0100

    update and delete functionality

--- src/Todo.js
+++ src/Todo.js
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 export default function Todo() {
   const [todoInput, setTodoInput] = useState("");
   const [todo, setTodo] = useState([]);
-  const [check, setCheck] = useState();
+  const [updateDisplay, setUpdateDisplay] = useState("none");
+  const [display, setDisplay] = useState("none");
   return (
     <div style={{ marginTop: 20 }}>
       <label style={{ fontSize: 23, fontWeight: "bold" }}>Enter Todo: </label>
@@ -38,34 +39,82 @@ export default function Todo() {
           return (
             <ul style={{ listStyle: "none" }}>
               <li
+                onClick={() => {
+                  display === "none" ? setDisplay("block") : setDisplay("none");
+                }}
                 style={{
                   borderStyle: "solid",
                   borderColor: "orange",
                   borderWidth: 1,
                   borderRadius: 5,
                   fontSize: 21,
-                  width: "80%"
+                  width: "80%",
+                  cursor: "pointer"
                 }}
               >
                 <input
                   type="checkbox"
                   onChange={() => {
-                    var index = todo.indexOf(e);
-
-                    if (index !== -1) {
-                      var newTodo = todo.splice(index, 1);
-                      setTodo([newTodo]);
-                    }
-
-                    // setCheck(true);
-
-                    // if(check === true){
-
-                    // }
+                    display === "none"
+                      ? setDisplay("block")
+                      : setDisplay("none");
                   }}
                 />{" "}
                 {e}
               </li>
+              <div style={{ display: display }}>
+                <input
+                  value="Edit"
+                  type="submit"
+                  onClick={() => {
+                    updateDisplay === "none"
+                      ? setUpdateDisplay("block")
+                      : setUpdateDisplay("none");
+                  }}
+                />
+                <input
+                  type="submit"
+                  value="Delete"
+                  onClick={() => {
+                    // var index = todo.indexOf(e);
+
+                    setTodo(
+                      todo.filter((item) => {
+                        return item !== e;
+                      })
+                    );
+                  }}
+                />
+              </div>
+              <div style={{ display: updateDisplay }}>
+                <input
+                  onChange={(e) => {
+                    setTodoInput(e.target.value);
+                  }}
+                  type="text"
+                  placeholder="Enter Todo Item"
+                  style={{
+                    marginTop: 20,
+                    width: 300,
+                    height: 40,
+                    borderRadius: 7,
+                    fontSize: 20,
+                    fontWeight: "bold",
+                    color: "cadetblue"
+                  }}
+                />
+                <input
+                  value="Update Todo"
+                  type="submit"
+                  onClick={() => {
+                    var index = todo.indexOf(e);
+                    todo[index] = todoInput;
+                    setTodo(todo);
+                    setTodoInput("");
+                    setUpdateDisplay("none");
+                  }}
+                />
+              </div>
             </ul>
           );
         })}
